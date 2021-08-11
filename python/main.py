2a197012138ed63d013f893b7d1388cb98334cbd
AmazingMech2418 <33498997+AmazingMech2418@users.noreply.github.com>
2021-08-11 06:48:10 -0400

    Add Python written out version

---
+++ python/main.py
@@ -0,0 +1,27 @@
+def toChurch(num):
+    res = lambda f : lambda x : x
+    for i in range(num):
+        res = (lambda n : lambda f : lambda x : f(n(f)(x)))(res)
+    return res
+
+
+Z = (lambda g : (lambda x : g(lambda v : x(x)(v)))(lambda x : g(lambda v : x(x)(v))))
+
+isZero = lambda n : n(lambda x : lambda a : lambda b: b)(lambda a : lambda b : a)
+
+_1 = lambda f : lambda x : f(x)
+_2 = lambda f : lambda x : f(f(x))
+_0 = lambda f : lambda x : x
+
+pred = lambda n : lambda f : lambda x : n(lambda g : lambda h : h(g(f)))(lambda u : x)(lambda u : u)
+
+mult = lambda m : lambda n : lambda f : lambda x : m(n(f))(x)
+
+F = lambda f : lambda n : isZero(n)(lambda v : _1)(lambda v : mult(n)(v(pred(n))))(f)
+#F = lambda f : lambda n : _1 if isZero(n)(True)(False) else mult(n)(f(pred(n)))
+
+
+
+factLambda = Z(F)
+
+print(factLambda(toChurch(5))(lambda n : n + 1)(0))
